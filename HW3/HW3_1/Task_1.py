24c35605fc3015c9195b91bd41fac41ad5b7b7e0
Demakova Tatiana <demakova_ts@mail.ru>
2022-10-18 16:00:25 +0700

    create HW3 task 1

---
+++ HW3/HW3_1/Task_1.py
@@ -0,0 +1,16 @@
+""" Задача 1. Создайте файл. Запишите в него N первых элементов последовательности Фибоначчи.
+6 –> 1 1 2 3 5 8 """
+def Fibonacci(n):
+    list_fib = [1,1]
+    #print(fib1, fib2, end=' ')
+    for i in range(2, n):
+        list_fib.append(list_fib[i-1]+list_fib[i-2])
+    return list_fib
+
+ 
+n = int(input("введите n = "))
+list_fibonacci = Fibonacci(n)
+
+file = open("fbonacci.txt", "w")
+print(*list_fibonacci, file=file, sep=", ")
+file.close()
