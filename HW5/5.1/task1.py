bb862b4f07cfbf98833081957667d6c55c166196
Demakova Tatiana <demakova_ts@mail.ru>
2022-10-25 23:08:34 +0700

    create HW5

---
+++ HW5/5.1/task1.py
@@ -0,0 +1,8 @@
+""" Задача 1. Задайте список случайных чисел от 1 до 10, выведите все элементы """
+
+import random as r
+list = [r.randint(1, 11)]
+n = r.randint(0, 10)
+for i in range(1, n):
+    list.append(r.randint(1, 10))
+print(f'список -> {list}')
