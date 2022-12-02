09af5008e62e91f81071b3cb64a8b79163d665ba
Demakova Tatiana <demakova_ts@mail.ru>
2022-12-02 14:30:50 +0700

    create HW8

--- HW8/task_8.3.py
+++ HW8/task_8.3.py
@@ -1,3 +1,41 @@
 # Задача 3. В двумерном массиве хранятся средние дневные температуры с мая по сентябрь за прошлый год.
 # Каждому месяцу соответствует своя строка. 
 # Определите самый жаркий и самый холодный 7-дневный промежуток этого периода. Выведите его даты.
+import random as r
+
+size = 5
+mas = [0] * size
+for i in range (size):
+    mas[i] = list(r.randint(1,15) for j in range(size))
+
+mas2=[]
+max_ave=0
+k=-1
+i_n=0 #индекс по строке начала промежутка
+i_k=0 #индекс по столбцу начала промежутка
+j_n=0 #индекс по строке конца промежутка
+j_k=0 #индекс по столбцу конца промежутка
+
+for i in range(0, len(mas)):
+    for i2 in range(0, len(mas[i])):
+        print(mas[i][i2], end=' ')
+    print()
+print()
+for i in range(0, len(mas)):
+    for j in range(0, len(mas[i])):
+        mas2.append(mas[i][j])
+        k+=1
+        if k>6:
+            ave=(mas2[k]+mas2[k-1]+mas2[k-2]+mas2[k-3]+mas2[k-4]+mas2[k-5]+mas2[k-6])/7
+            if ave>max_ave:
+                max_ave=ave
+                i_k=i
+                j_k=j
+                if j<6:
+                    i_n=i-1
+                    j_n=len(mas[i_n])-(6-j)
+                else:
+                    i_n=i
+                    j_n=j-6
+                    
+print ('7 дневный промежуток, начало - месяц:',i_n+1,'день:',j_n+1,'конец промежутка - месяц:',i_k+1,'день:',j_k+1, 'максимальная температура ->', max_ave)
